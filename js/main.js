aba137b6d16b79beeeb1a15c9c30a8b3349956c3
jeanpaul1304 <jeanpaul1304>
2018-10-14 17:04:29 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let menubtn = document.querySelector('.menu-btn')
+let nav = document.querySelector('.nav-ctn')
+let name = document.querySelector('#email')
+let login = document.querySelector('.login-ctn')
+let mapa = document.querySelector('.map-ctn')
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    nav.classList.add('show')
+})
+
+document.querySelector('.btn-hide').addEventListener('click', () => {
+    document.querySelector('.nav-ctn').classList.remove('show')
+})
+
+document.querySelector('.btn-login').addEventListener('click', () => {
+    if (name.value == 'cliente@mail.com') {
+        document.querySelector('.login-ctn').remove()
+        mapa.classList.remove('hide')
+    }
+})
